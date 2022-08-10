601d46949af4874ae50ae80a317f9f05ba3ad654
Reshma <Manjunath>
2022-08-10 14:52:49 +0530

    commited

---
+++ Java_Pgn/src/wed/AppendAt0.java
@@ -0,0 +1,21 @@
+package wed;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AppendAt0 {
+	public static void main(String[] args)
+	{
+	int[] a={0,1,0,0,1,1,1,0,0};
+	
+	ArrayList<Integer> set=new ArrayList<>();
+	for (int j = 0; j < a.length; j++) {
+		set.add(a[j]);
+	}
+	for (Integer integer : set) {
+		System.out.println(integer);
+	}
+
+	
+	}
+}
